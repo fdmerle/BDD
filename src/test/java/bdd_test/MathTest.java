44ef39ad151b5481f3fe1ba57050ab3ee9ca0fcb
unknown <dmytro_moskalenko2@EPPLKRAW0218.budapest.epam.com>
2016-02-01 13:10:55 +0100

    Simpae_example

---
+++ src/test/java/bdd_test/MathTest.java
@@ -0,0 +1,12 @@
+package bdd_test;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format = "junit:target/junit.xml")
+public class MathTest {
+
+}
